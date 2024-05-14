198c4cb3a15663654bb034f54b898ea5923f1200
Rose-Merline Reveil <rreveil@uci.edu>
2024-05-14 01:33:57 -0400

    Correcting syntax mistakes

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ let port=process.env.PORT;
 let directory=__dirname + '/public';
 
 /* if we are not running on Heroku, the n we need to adjust our port and directory */
-if ((typeof (port== 'undefined') || (port === null))) {
+if ((typeof port== 'undefined') || (port === null)) {
    port=8080;
    directory= './public';
 }
@@ -21,14 +21,13 @@ let file = new static.Server(directory);
 
 let app=http.createServer(
     function(request, response){
-    request.addListener(
-        'end', 
+    request.addListener('end', 
         function(){
             file.serve(request, response);
         }
     ).resume();
     
     }    
-).listen(port || 5000);
+).listen(port);
 
 console.log('The server is running');
